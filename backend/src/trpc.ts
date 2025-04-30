2fb0719fbb30c75b28bdca3021e9ccb482c0769c
Sergey Milos <sergeymilos52@gmail.com>
2025-04-30 20:43:15 +0300

    eslint dont work

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -10,6 +10,8 @@ const ideas = [
 
 const trpc = initTRPC.create()
 
+if (true) console.log(12)
+
 export const trpcRouter = trpc.router({
   getIdeas: trpc.procedure.query(() => {
     return { ideas }
